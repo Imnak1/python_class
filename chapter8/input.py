83d95616150a6f2a777ff08c520c9ccc64d636a9
Kanmi98 <adekanmi2009@gmail.com>
2026-01-22 00:13:09 +0100

    add string format

---
+++ chapter8/input.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+k = [1]
+print(k)
+
+#exercise on fstring
+name = "John" 
+age = 21 
+print ("My name is {} and I am {} years old.".format(name,age)) 
+
+#String formatting with arguments
+print("Hello. My name is {name}, and I am a {occupation}.".format(occupation = "programmer", name = "John"))
+
+
