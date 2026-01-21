babc851d2738e144fe7aa7983671166ea31fce70
Kanmi98 <adekanmi2009@gmail.com>
2026-01-22 00:28:16 +0100

    add input

--- chapter8/input.py
+++ chapter8/input.py
@@ -11,3 +11,22 @@ print ("My name is {} and I am {} years old.".format(name,age))
 print("Hello. My name is {name}, and I am a {occupation}.".format(occupation = "programmer", name = "John"))
 
 
+#exercise on input 
+
+name = str(input("Enter your name: ")) 
+
+age = int(input("How old are you?: ")) 
+
+sex = str(input("Enter your gender M or F: ")) 
+
+location = str(input("Which city do you live in: ")) 
+
+if sex == "M" or "m": 
+    gender = "male" 
+elif sex == "F" or "f": 
+    gender = "female" 
+else: 
+    gender = "invalid" 
+
+print ("{}, you are a {} old {} from {}.".format(name, age, gender, location))
+print (f"{name}, you are a {age} old {gender} from {location}.")
